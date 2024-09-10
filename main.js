13b8f47f360a5114c5741de80bc397e8dc4c543b
Gal Ben David <gal.benda3@gmail.com>
2024-09-10 16:07:56 +0300

    Update onBallClick()

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 'use strict'
 
-function onBallClick() {
-    console.log('hi')
+function onBallClick(elBall) {
+    elBall.style.width = 150 + 'px'
+    elBall.style.height = 150 + 'px'
+
+    elBall.innerText = '150'
 }

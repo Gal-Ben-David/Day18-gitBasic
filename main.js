558695deeeccd97f5f16579db93a79f32d724a54
Gal Ben David <gal.benda3@gmail.com>
2024-09-10 16:23:34 +0300

    Limit the ball dSize

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 'use strict'
 
-function onBallClick(elBall) {
-    elBall.style.width = 150 + 'px'
-    elBall.style.height = 150 + 'px'
+var gBallSize = 100
 
-    elBall.innerText = '150'
+function onBallClick(elBall) {
+    gBallSize += 50
+
+    if (gBallSize > 400) {
+        gBallSize = 100
+    }
+
+    elBall.style.width = gBallSize + 'px'
+    elBall.style.height = gBallSize + 'px'
+
+    elBall.innerText = gBallSize
+
 }
